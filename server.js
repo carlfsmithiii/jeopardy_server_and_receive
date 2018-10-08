2d537d270f73646bc6ea5850fdb46a1bb82739cc
Carl Smith <carlfsmithiii@gmail.com>
2018-10-08 16:39:09 -0400

    changed so that the disk is accessed for each get request

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const fs = require('fs');
-const categories = require('./data/categories.json');
+const jserviceInit = require('./jservice-init');
+const categoriesFilePath = './categories.json';
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+jserviceInit.writeToDisk();
+
+const categoriesArray = [67, 780, 277, 223, 184, 680, 21, 309, 582, 267, 136, 249, 105, 770, 508, 561, 420, 37, 1195, 25, 897];
+
 
 app.get('/api/category/:id', (request, response) => {
+    const json = fs.readFileSync('data/categories.json');
+    const categories = JSON.parse(json);
     const requestedCategory = categories.find(category => category.id == request.params.id);
     response.send(requestedCategory);
 });
