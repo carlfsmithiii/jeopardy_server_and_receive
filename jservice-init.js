085ad99d919428d987dbea0b85cc34aa12e43bb9
Carl Smith <carlfsmithiii@gmail.com>
2018-10-10 22:55:20 -0400

    updated css -- font sizes

--- jservice-init.js
+++ jservice-init.js
@@ -8,7 +8,7 @@ async function getCategoryFromJService(categoryNumber) {
     return response.data;
 }
 
-const url = 'http://jservice.io/api/category?id=';
+// const url = 'http://jservice.io/api/category?id=';
 const categoriesArray = [67, 780, 277, 223, 184, 680, 21, 309, 582, 267, 136, 249, 105, 770, 508, 561, 420, 37, 1195, 25, 897];
 
 const categoryPromises = categoriesArray.map(id => getCategoryFromJService(id))
--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+
+// const categoriesArray = [67, 780, 277, 223, 184, 680, 21, 309, 582, 267, 136, 249, 105, 770, 508, 561, 420, 37, 1195, 25, 897];
+// async function doit() {
+//     await jserviceInit.writeToDisk();
+
+//     app.get('/api/category/:id', (request, response) => {
+//         const json = fs.readFileSync('data/categories.json');
+//         const categories = JSON.parse(json);
+//         const requestedCategory = categories.find(category => category.id == request.params.id);
+//         response.send(requestedCategory);
+//     });
+// }
+// doit();
 jserviceInit.writeToDisk();
-
-const categoriesArray = [67, 780, 277, 223, 184, 680, 21, 309, 582, 267, 136, 249, 105, 770, 508, 561, 420, 37, 1195, 25, 897];
-
 
 app.get('/api/category/:id', (request, response) => {
     const json = fs.readFileSync('data/categories.json');
